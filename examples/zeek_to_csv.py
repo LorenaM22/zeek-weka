37b5cdc282a66743c9088b067e80aa98f386d207
LorenaM22 <77121705+LorenaM22@users.noreply.github.com>
2022-07-07 10:46:11 +0200

    Update zeek_to_csv.py

--- examples/zeek_to_csv.py
+++ examples/zeek_to_csv.py
@@ -2,6 +2,6 @@ from parsezeeklogs import ParseZeekLogs
 import sys 
 
 with open('out.csv',"w") as outfile:
-    for log_record in ParseZeekLogs(sys.argv[2], output_format="csv", safe_headers=False, fields=["ts", "uid","id.orig_h","id.orig_p","id.resp_h","id.resp_p", "proto", "duration", "orig_bytes", "resp_bytes", "conn_state", "local_orig", "local_resp", "missed_bytes", "history", "orig_pkts", "orig_ip_bytes", "resp_pkts", "resp_ip_bytes"]):
+    for log_record in ParseZeekLogs(sys.argv[1], output_format="csv", safe_headers=False, fields=["ts", "uid","id.orig_h","id.orig_p","id.resp_h","id.resp_p", "proto", "duration", "orig_bytes", "resp_bytes", "conn_state", "local_orig", "local_resp", "missed_bytes", "history", "orig_pkts", "orig_ip_bytes", "resp_pkts", "resp_ip_bytes"]):
         if log_record is not None:
             outfile.write(log_record + "\n")
