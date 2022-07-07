89c71b9093fdf5a88c64f03649c7fdc0cff0fabe
LorenaM22 <77121705+LorenaM22@users.noreply.github.com>
2022-07-07 10:48:02 +0200

    Delete load_zeek_log_json.py

--- examples/load_zeek_log_json.py
+++
@@ -1,6 +0,0 @@
-from parsezeeklogs import ParseZeekLogs
-
-# Print the field line out
-for log_record in ParseZeekLogs("conn.log", output_format="json", safe_headers=False):
-    if log_record is not None:
-        print(str(log_record))
